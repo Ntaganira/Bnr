4a2b303a33903c64bc8f3aa732cda8e56a33accf
Heritier NTAGANIRA <30772923+Ntaganira@users.noreply.github.com>
2026-05-11 19:30:00 +0200

    Initialize Spring Boot project with H2 configuration

---
+++ src/main/java/rw/bnr/heritier/HeritierApplication.java
@@ -0,0 +1,13 @@
+package rw.bnr.heritier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeritierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeritierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rw/bnr/heritier/HeritierApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.bnr.heritier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeritierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
